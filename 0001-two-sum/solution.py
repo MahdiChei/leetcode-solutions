3620400c7ccc53dd161e58b1d59302b7a2c88bad
MahdiChei <61237707+MahdiChei@users.noreply.github.com>
2026-09-19 15:23:41 +0100

    Delete 0001-two-sum directory

--- 0001-two-sum/solution.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        listLen = len(nums)
-        for i in range(0,listLen):
-            try:
-                searchVal = target - nums[i]
-                return [nums.index(searchVal, i+1),i]
-            except:
-                print("EL Mahdi CHEIKH")
